e30db16b10bb29238388ab81f4c9f10d0ecb6eba
Leonardo <leobusta@gmail.com>
2019-09-24 17:59:26 -0500

    Clase 19 Sept 2019

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import VueRouter from 'vue-router';
+
 import App from './App';
 
+import Home from './views/Home';
+import Pets from './views/Pets';
+import Favorites from './views/Favorites';
+import store from './store';
+
 Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/pets', component: Pets },
+  { path: '/favorites', component: Favorites },
+];
+
+const router = new VueRouter({ routes });
 
 Vue.config.productionTip = false;
 
@@ -12,4 +28,6 @@ new Vue({
   el: '#app',
   components: { App },
   template: '<App/>',
+  router,
+  store,
 });
---
+++ src/views/Favorites.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <v-list>
+          <v-subheader> My Favorites </v-subheader>
+          <v-list-tile avatar v-for="(dog, index) in favorites" :key="index" >
+              <v-list-tile-avatar >
+                  <img :src="dog.img">
+              </v-list-tile-avatar>
+              <v-list-content>{{dog.name}}</v-list-content>
+          </v-list-tile>
+      </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    favorites() {
+      return this.$store.state.favorites;
+    },
+  },
+
+};
+</script>
+
+<style>
+
+</style>
